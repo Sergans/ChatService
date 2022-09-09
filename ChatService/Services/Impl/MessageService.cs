a61151e50a656cd1e7e0f4dbe8710210902a1f85
Sergans <gan7er@yandex.ru>
2022-09-09 08:54:09 +0300

    add service

---
+++ ChatService/Services/Impl/MessageService.cs
@@ -0,0 +1,24 @@
+﻿using ChatService.Models;
+using System.Diagnostics.Metrics;
+
+namespace ChatService.Services.Impl
+{
+    public class MessageService : IMessageService
+    {
+        private int _counter = 1000;
+        private Dictionary<int, Client> _clients = new Dictionary<int, Client>();
+        public int AddClient(string name)
+        {
+
+            lock (_clients)
+            {
+                _clients[++_counter] = new Client
+                {
+                    Id = _counter,
+                    Name = name
+                };
+            }
+            return _counter;
+        }
+    }
+}
